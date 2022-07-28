0e475e4493e4a25ed0774c696a4f6bdb0dc8d80f
Nelson Hernández <50471203+nelsondev19@users.noreply.github.com>
2022-07-27 21:20:02 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,7 +79,7 @@ const Home: NextPage = () => {
                   d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
                 />
               </svg>
-              <span className="ml-1">Open Editor</span>
+              <span className="ml-1">Editor</span>
             </a>
           </div>
           <div className="text-base text-center md:text-left">
